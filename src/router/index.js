1991d98c020bc30487d472a9a57f1309aa4c2424
mohamaddika <mochandikarustandi@gmail.com>
2023-09-08 14:23:15 +0700

    push frontend

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
-function cekToken(to, from, next) {
-  if (!!localStorage.getItem('token') && localStorage.getItem('token') != 'undefined') {
-    next()
-  } else {
-    alert('Login Heula Atuh Broo!')
-    next('/login')
-  }
-};
-function CekProfil(to, from, next) {
-  if (!!localStorage.getItem('token') && localStorage.getItem('token') != 'undefined') {
-    next()
-  } else {
-    next('/')
-  }
-};
-
-
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'Login',
-      component: () => import('../views/LoginView.vue'),
-    },
     {
       path: '/admin/dashboard',
       name: 'Dashboard',
